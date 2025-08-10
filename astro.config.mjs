727ca983b6f4eaa546276162f74f6c68790a746c
Meshari305 <mesharinowaf@gmail.com>
2025-08-10 09:43:14 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/static'; // ✅ static adapter
 
 export default defineConfig({
   integrations: [tailwind()],
-  adapter: vercel(),
-  site: 'https://workforce.example' // update later to https://workforce.sa
+  site: 'https://workforce.example'
 });
- 
